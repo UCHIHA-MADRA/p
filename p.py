02a71ac345deeaf9112dbded4d0dcb28a3a7a5c0
UCHIHAMADRA <68822532+UCHIHA-MADRA@users.noreply.github.com>
2022-09-30 10:30:03 +0530

    Add files via upload

---
+++ p.py
@@ -0,0 +1,12 @@
+name = input('Enter your name: ')
+age = input('Enter your age: ')
+
+year = 2022 - int(age) + 100
+message = name + ' will be 100 years old in ' + str(year) + '\n'
+print(message)
+
+
+num = input('Enter a number: ')
+message = message * int(num)
+
+print(message)
